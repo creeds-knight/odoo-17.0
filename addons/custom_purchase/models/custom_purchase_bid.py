b80013e8f78358d61ac633912da1c0259011e239
creeds-knight <apedoarthur21@gmail.com>
2024-10-19 15:50:15 +0300

    Added methods to select bid

--- addons/custom_purchase/models/custom_purchase_bid.py
+++ addons/custom_purchase/models/custom_purchase_bid.py
@@ -1,9 +1,5 @@
-"""
-        This model describes how Bids interacts with RFQ's and vendors
-"""
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-
 
 class PurchaseBid(models.Model):
     _name = 'custom.purchase.bid'
@@ -11,8 +7,7 @@ class PurchaseBid(models.Model):
 
     name = fields.Char('Bid reference', required=True, default='Unknown')
     order_id = fields.Many2one('custom.purchase.order', string='RFQ')
-    vendor_id = fields.Many2one('res.partner', string='Vendor',
-                                required=True, default='Not Provided')
+    vendor_id = fields.Many2one('res.partner', string='Vendor', required=True)
     bid_amount = fields.Float('Bid Amount', required=True, default=0)
     bid_date = fields.Datetime('Bid Date', default=fields.Datetime.now)
     is_winning_bid = fields.Boolean('Winning Bid', default=False)
@@ -20,12 +15,12 @@ class PurchaseBid(models.Model):
     @api.onchange('is_winning_bid')
     def _onchange_is_winning_bid(self):
         for bid in self:
-            if bid.is_winning_bid:
+            if bid.is_winning_bid and bid.id:
                 other_bids = self.search([
-                        ('order_id', '=', self.order_id.id),
-                        ('is_winning_bid', '=', True),
-                        ('id', '!=', self.id)
-                    ])
+                    ('order_id', '=', bid.order_id.id),
+                    ('is_winning_bid', '=', True),
+                    ('id', '!=', bid.id)
+                ])
                 if other_bids:
                     other_bids.write({'is_winning_bid': False})
 
@@ -34,14 +29,14 @@ class PurchaseBid(models.Model):
         for bid in self:
             if bid.is_winning_bid:
                 other_winning_bids = self.search([
-                        ('order_id', '=', self.order_id.id),
-                        ('is_winning_bid', '=', True),
-                        ('id', '!=', self.id)
-                    ])
+                    ('order_id', '=', bid.order_id.id),
+                    ('is_winning_bid', '=', True),
+                    ('id', '!=', bid.id)
+                ])
                 if other_winning_bids:
-                    raise ValidationError('Only one winning bid is \
-                                          allowed per RFQ.')
+                    raise ValidationError('Only one winning bid is allowed per RFQ.')
                 bid.order_id.update({
-                        'partner_id': bid.vendor_id.id,
-                         'amount_total': bid.bid_amount
-                    })
+                    'partner_id': bid.vendor_id.id,
+                    'amount_total': bid.bid_amount
+                })
+
