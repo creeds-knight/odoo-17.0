c62bce0df007d89056ebf8546559f3be2d7add08
creeds-knight <apedoarthur21@gmail.com>
2024-10-21 13:35:51 +0300

    adding method action to send rfq

--- addons/custom_purchase/models/custom_purchase_order.py
+++ addons/custom_purchase/models/custom_purchase_order.py
@@ -1,19 +1,19 @@
 """
-    This module modifies the Purchase order model in the Purchases application
+This module modifies the Purchase order model in the Purchases application
 """
 from odoo import models, fields
 
-
 class CustomPurchaseOrder(models.Model):
-    _name = 'custom.purchase.order'
     _inherit = 'purchase.order'
 
     vendor_ids = fields.Many2many('res.partner', string='Vendors',
-                                  domain=[('supplier_rank', '>', 0)])
+                                   domain=[('supplier_rank', '>', 0)])
 
     def action_rfq_send(self):
         for vendor in self.vendor_ids:
             rfq_copy = self.copy(default={
                 'partner_id': vendor.id,
-                })
+                'state': 'draft',
+            })
             rfq_copy.button_confirm()
+
