57b57fcc60a26990f7f1b625c03f3c3364c323d4
creeds-knight <apedoarthur21@gmail.com>
2024-10-28 18:13:18 +0300

    NIN debug check

--- addons/id_application/controllers/controllers.py
+++ addons/id_application/controllers/controllers.py
@@ -1,6 +1,8 @@
 from odoo import http
 from odoo.http import request
 import base64
+import random
+import string
 
 class IdApplicationController(http.Controller):
 
@@ -8,9 +10,12 @@ class IdApplicationController(http.Controller):
     def id_application_form(self, **kwargs):
         return http.request.render('id_application.national_id_application_form',{})
 
-    @http.route('/national_id/application/submit', type='http', auth='none', methods=['POST'], csrf=True)
+    @http.route('/national_id/application/submit', type='http', auth='public', methods=['POST'], csrf=True)
     def submit_application(self, **post):
         try:
+            #sequence = request.env['ir.sequence'].next_by_code('id.application')
+            #random_alpha = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+            #nin = f'{sequence}{random_alpha}' if sequence else 'N/A'
             lc_letter_file = post.get('lc_letter')
             photo_file = post.get('photo')
             lc_letter = base64.b64encode(lc_letter_file.read()) if lc_letter_file else False
@@ -23,9 +28,15 @@ class IdApplicationController(http.Controller):
                     'parent_nin': post.get('parent_nin'),
                     'lc_letter': lc_letter,
                     'photo': photo,
+                    'stage': 'submitted',
+                    #'nin': nin,
 
                     }
             application = request.env['id.application'].sudo().create(vals)
             return request.render('id_application.thank_you', {'application': application})
         except Exception as e:
             return request.render('id_application.error', {'error': str(e)})
+
+
+
+
